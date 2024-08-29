aa2ce4211a0b410ed438fa3edd32724a48d1cda6
kad_1 <kad_16@live.com>
2024-08-29 11:05:56 -0700

    lab 1 finish

---
+++ lab1.py
@@ -0,0 +1,35 @@
+'kadeyen'
+'anderson'
+print(len('kadeyen ' + 'anderson') )
+print ('kadeyen'.upper(), 'anderson'.lower())
+
+
+
+age = 30
+weight = 200
+height = 6.0
+print(type (age))
+print(type (weight))
+print(type (height))
+inches = 12
+import math
+BMI = (weight/(6*12)**2)*703
+print(BMI)
+
+import math
+radius =5
+area  = math.pi * radius **2
+print("the area of the circle with", radius, 'is' ,area)
+radius = 3
+volume = (3/4) * math.pi **3
+print('the volume of the sphere with', radius, 'is' ,volume )
+A= 3
+B= 6
+import math
+c= (A**2+B**2)**2
+print(c)
+
+
+
+
+
---
+++ tt.py
@@ -0,0 +1,10 @@
+
+'Hello'
+'world'
+"it's a small "
+'Well, '
+print('well, ' + 'hell0 ' + "it's a small " + 'world.')
+'spam, ' * 4
+len('spam')
+
+
